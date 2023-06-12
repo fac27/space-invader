1729a92bc6820ffd5bfe8e324178a6967062dda8
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-06-12 17:43:02 +0100

    dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,11 @@
-module.exports = {
+exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'react-minimal'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
