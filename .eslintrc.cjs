357c44a2722fdcdea9dc736068ec5cd1fb49a295
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-06-13 10:46:46 +0100

    eslint rule changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,12 +5,12 @@ exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'kentcdodds'
+    'react-minimal'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react-refresh/only-export-components': 'off',
   },
 }
